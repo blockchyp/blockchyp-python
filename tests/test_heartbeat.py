5f9707e26ba6b56bb2986f9e6bd5e8de43014408
Andrew Hutson <andrew.hutson@outlook.com>
2019-07-08 14:12:13 -0700

    Add docstrings and adjust pylint settings

--- tests/test_heartbeat.py
+++ tests/test_heartbeat.py
@@ -1,11 +1,13 @@
-import pytest
+"""This file contains unit tests for the BlockChypClient heartbeat method"""
+
 
 import blockchyp
 
 def test_heartbeat():
+    """This method tests functionality of the heartbeat method"""
     creds = blockchyp.BlockChypCredentials("", "", "")
     client = blockchyp.BlockChypClient(creds)
     reply = client.heartbeat()
 
     assert reply["success"]
-    assert reply["errors"] == None
+    assert reply["errors"] is None
