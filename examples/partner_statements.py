6b4c3942a7f455acdeecfe0811d31a843a8a542b
BlockChyp SDK Builder <devops@blockchyp.com>
2023-10-30 10:39:39 -0600

    Merge pull request #253 from blockchyp/develop
    Develop

---
+++ examples/partner_statements.py
@@ -0,0 +1,19 @@
+import os
+
+import blockchyp
+
+# initialize a client.
+client = blockchyp.Client(
+    api_key=os.environ["BC_API_KEY"],
+    bearer_token=os.environ["BC_BEARER_TOKEN"],
+    signing_key=os.environ["BC_SIGNING_KEY"],
+)
+
+# populate request parameters.
+request = {
+}
+
+# run the transaction.
+response = client.partner_statements(request)
+
+print("Response: %r" % response)
---
+++ tests/integration/partner_statements_test.py
@@ -0,0 +1,36 @@
+# Copyright 2019-2023 BlockChyp, Inc. All rights reserved. Use of this code is
+# governed by a license that can be found in the LICENSE file.
+#
+# This file was generated automatically by the BlockChyp SDK Generator. Changes
+# to this file will be lost every time the code is regenerated.
+import os
+import os.path
+import time
+import uuid
+import pkg_resources
+
+import pytest
+
+import blockchyp
+
+from .util import _get_test_client, _get_test_config
+
+
+@pytest.mark.itest
+def test_partner_statements():
+    """Can list partner statements."""
+
+
+    terminal = _get_test_config().get("defaultTerminalName")
+
+
+    client = _get_test_client("")
+
+    request = {
+        "test": True,
+    }
+
+    response = client.partner_statements(request)
+    print("Response: %r" % response)
+
+    assert response.get("success") is True
