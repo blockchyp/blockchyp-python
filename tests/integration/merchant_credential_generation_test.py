d31465a62f0d42dabad7fdba3667c96f7020168e
BlockChyp SDK Builder <devops@blockchyp.com>
2024-02-01 18:13:02 +0000

    Merge pull request #260 from blockchyp/feature/javaBug
    Java test bug and php null exception fix

---
+++ tests/integration/merchant_credential_generation_test.py
@@ -0,0 +1,37 @@
+# Copyright 2019-2024 BlockChyp, Inc. All rights reserved. Use of this code is
+# governed by a license that can be found in the LICENSE file.
+#
+# This file was generated automatically by the BlockChyp SDK Generator. Changes
+# to this file will be lost every time the code is regenerated.
+import os
+import os.path
+import time
+import uuid
+import pkg_resources
+
+import pytest
+
+import blockchyp
+
+from .util import _get_test_client, _get_test_config
+
+
+@pytest.mark.itest
+def test_merchant_credential_generation():
+    """Can generate merchant api credentials."""
+
+
+    terminal = _get_test_config().get("defaultTerminalName")
+
+
+    client = _get_test_client("")
+
+    request = {
+        "test": True,
+        "merchantId": "<MERCHANT ID>",
+    }
+
+    response = client.merchant_credential_generation(request)
+    print("Response: %r" % response)
+
+    assert response.get("success") is True
