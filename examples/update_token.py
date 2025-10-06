1ad221d1ade6204c0e319d1c57468fedcd8d6f22
BlockChyp SDK Builder <devops@blockchyp.com>
2025-10-06 21:03:44 +0000

    Merge pull request #342 from blockchyp/task/VIP-473
    Task/vip 473 - Added the updateToken route to the SDKs

---
+++ examples/update_token.py
@@ -0,0 +1,22 @@
+import os
+
+import blockchyp
+
+# initialize a client.
+client = blockchyp.Client(
+    api_key=os.environ["BC_API_KEY"],
+    bearer_token=os.environ["BC_BEARER_TOKEN"],
+    signing_key=os.environ["BC_SIGNING_KEY"],
+)
+
+# populate request parameters.
+request = {
+    "token": "<TOKEN>",
+    "expMonth": 12,
+    "expYear": 2040,
+}
+
+# run the transaction.
+response = client.update_token(request)
+
+print("Response: %r" % response)
