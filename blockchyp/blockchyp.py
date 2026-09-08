d0981243d8ce97d76013e1f73ad8deca291faf4c
BlockChyp SDK Builder <devops@blockchyp.com>
2026-09-08 17:39:02 +0000

    Merge pull request #381 from blockchyp/VIP-1443-Service-fee-endpoint
    feat(VIP-1443): service fee endpoint

--- blockchyp/blockchyp.py
+++ blockchyp/blockchyp.py
@@ -517,6 +517,32 @@ class Client:
 
         return response
 
+    def service_fee(self, request):
+        # type: (dict) -> dict
+        """Calculates the service fee for a transaction."""
+
+        self._populate_signature_options(request)
+
+        if self._is_terminal_routed(request.get("terminalName")):
+            response = self._terminal_request(
+                method="POST",
+                path="/api/service-fee",
+                body=request,
+                terminal=request.get("terminalName"),
+            )
+        else:
+            response = self._gateway_request(
+                method="POST",
+                path="/api/service-fee",
+                body=request,
+                test=request.get("test", False),
+                relay=True,
+            )
+
+        self._handle_signature(request, response)
+
+        return response
+
     def list_queued_transactions(self, request):
         # type: (dict) -> dict
         """Returns a list of queued transactions on a terminal."""
